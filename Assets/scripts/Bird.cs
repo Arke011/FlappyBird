e1b0b026ee613354c2461f74453760db198dd44d
Arke011 <146362737+Arke011@users.noreply.github.com>
2023-10-12 19:30:37 +0300

    pipe movement

---
+++ Assets/scripts/Bird.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float jumpSpeed;
+    public float rotatePower;
+    Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            rb.velocity = Vector2.up * jumpSpeed;
+        }  
+
+        transform.eulerAngles = new Vector3(0, 0, rb.velocity.y * rotatePower);
+    }
+
+}

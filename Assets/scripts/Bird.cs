9b73963b3d3e973f52893883a91967a5809b68be
Arke011 <146362737+Arke011@users.noreply.github.com>
2023-10-12 20:07:54 +0300

    good

--- Assets/scripts/Bird.cs
+++ Assets/scripts/Bird.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System;
+
 
 public class Bird : MonoBehaviour
 {
+    public TMP_Text birdScoreText;
     public float jumpSpeed;
     public float rotatePower;
+    int birdScore = 0;
     Rigidbody2D rb;
     
     void Start()
@@ -22,6 +28,24 @@ public class Bird : MonoBehaviour
         }  
 
         transform.eulerAngles = new Vector3(0, 0, rb.velocity.y * rotatePower);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Die();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        birdScore++;
+        birdScoreText.text = birdScore.ToString();
+    }
+
+    void Die()
+    {
+        var sceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(sceneName);
     }
 
 }
